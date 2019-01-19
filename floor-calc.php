cc9511644db24be763acc9df54f0fc85a8638241
Orion55 <grol55@mail.ru>
2019-01-19 21:41:13 +0500

    first commit

---
+++ floor-calc.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: Floor Calc
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: Плагин калькулятора по расчету стоимости услуг клининговой компании
+Version: 1.0
+Author: Grebenev Oleg
+Author URI: http://portfolio.infoblog72.ru/
+License: GPL2
+*/
+
+defined('ABSPATH') or die('Nope, not accessing this');
+
+include(plugin_dir_path(__FILE__) . 'inc/admin_options.php');
