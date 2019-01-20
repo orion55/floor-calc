02566843d8ada1c069484bd66d26ca48dfd73fb3
Orion55 <grol55@mail.ru>
2019-01-20 17:55:02 +0500

    vue-cli

---
+++ shortcode/src/App.vue
@@ -0,0 +1,21 @@
+/* eslint-disable */
+<template>
+    <div id="floor-calc2">
+        <h1>Hello floor!!!</h1>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style lang="scss">
+    #floor-calc2 {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        text-align: center;
+        color: coral;
+        margin-top: 60px;
+    }
+</style>
