cb85456f25a6ddfbbaa744ed9b722a5051c7a55e
Orion55 <grol55@mail.ru>
2019-01-20 21:56:55 +0500

    Объект клининга

--- shortcode/src/App.vue
+++ shortcode/src/App.vue
@@ -1,21 +1,39 @@
 /* eslint-disable */
 <template>
-    <div id="floor-calc2">
-        <h1>Hello floor!!!</h1>
+    <div class="floor-form">
+        <multiselect v-model="value" :options="options" :searchable="false" :close-on-select="false" :show-labels="false"></multiselect>
     </div>
 </template>
 
 <script>
+  import Multiselect from 'vue-multiselect'
+
+  Vue.component('multiselect', Multiselect)
+
   export default {
-    name: 'app'
+    name: 'app',
+    data () {
+      return {
+        value: 'квартира',
+        options: ['квартира', 'коттедж', 'офис']
+      }
+    }
   }
 </script>
 
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
 <style lang="scss">
-    #floor-calc2 {
-        font-family: "Avenir", Helvetica, Arial, sans-serif;
-        text-align: center;
-        color: coral;
-        /*margin-top: 60px;*/
+    .floor-form {
+        position: absolute;
+        top: 50%;
+        left: auto;
+        width: 290px;
+        height: 430px;
+        margin: 0;
+        padding: 30px;
+        background-color: rgba(255, 255, 255, .9);
+        box-shadow: 5px 5px 11px rgba(0, 0, 0, .1);
+        transform: translateY(-50%);
     }
 </style>
