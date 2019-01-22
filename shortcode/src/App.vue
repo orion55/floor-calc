ff4d8dc8bd83e4b40b317a3558262dfe7d667c48
Orion55 <grol55@mail.ru>
2019-01-22 22:56:53 +0500

    вёрстка дизайна

--- shortcode/src/App.vue
+++ shortcode/src/App.vue
@@ -3,7 +3,7 @@
         <div v-show="info.loading" class="calc__preloader">
             <div class="calc__loader"></div>
         </div>
-        <div v-show="!info.loading">
+        <div v-show="!info.loading" class="calc__box">
             <div class="calc__head">Объект клининга</div>
             <multiselect v-model="objectCleaning.selected" :options="objectCleaning.options"
                          label="name" track-by="id" :searchable="false"
@@ -28,6 +28,12 @@
                          :show-labels="false" :maxHeight="200"
                          class="calc__dropdown calc__dropdown--cleaning"
                          :allow-empty="false"></multiselect>
+            <div class="calc__services hvr-pop"> Дополнительные услуги <span class="calc__plus">&#43;</span></div>
+            <div class="calc__price">от <span class="calc__sum">1000</span> ₽</div>
+            <button type="button" class="btn btn--result hvr-pop"
+                    @click.prevent="buttonCheckout.funct"
+                    ref="btnCheckout"> Заказать
+            </button>
         </div>
     </div>
 </template>
@@ -94,99 +100,210 @@
 <style lang="scss">
     $color-main: #F25E99;
     $color-two: darken($color-main, 10%);
-
-    .calc__form {
-        position: absolute;
-        top: 50%;
-        left: auto;
-        width: 290px;
-        height: 430px;
-        margin: 0;
-        padding: 15px;
-        background-color: rgba(255, 255, 255, .9);
-        box-shadow: 5px 5px 11px rgba(0, 0, 0, .1);
-        transform: translateY(-50%);
-    }
-
-    .calc__loader {
-        border: 5px solid #f3f3f3;
-        border-top: 5px solid $color-main;
-        border-radius: 50%;
-        width: 40px;
-        height: 40px;
-        animation: spin 2s linear infinite;
-    }
-
-    @keyframes spin {
-        0% {
-            transform: rotate(0deg);
+    #floor-calc {
+        .calc__form {
+            position: absolute;
+            top: 50%;
+            left: 15%;
+            width: 290px;
+            height: 430px;
+            margin: 0;
+            padding: 15px;
+            background-color: rgba(255, 255, 255, .9);
+            box-shadow: 5px 5px 11px rgba(0, 0, 0, .1);
+            transform: translateY(-50%);
+            z-index: 100;
         }
-        100% {
-            transform: rotate(360deg);
+
+        .calc__loader {
+            border: 5px solid #f3f3f3;
+            border-top: 5px solid $color-main;
+            border-radius: 50%;
+            width: 40px;
+            height: 40px;
+            animation: spin 2s linear infinite;
+        }
+
+        @keyframes spin {
+            0% {
+                transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+
+        .calc__preloader {
+            display: flex;
+            flex: 1;
+            align-items: center;
+            justify-content: center;
+            height: 100%;
+        }
+
+        .multiselect__content {
+            padding-left: 0;
+        }
+
+        .multiselect {
+            min-height: 33px;
+        }
+
+        .multiselect__tags {
+            min-height: 33px;
+            padding: 3px 33px 0 5px;
+            border: 1px solid rgba(60, 60, 60, .26);
+        }
+
+        .multiselect__select {
+            height: 33px;
+        }
+
+        .multiselect__content-wrapper {
+            z-index: 40;
+        }
+
+        .multiselect__element {
+            margin-bottom: 0;
+            z-index: 100;
+        }
+
+        .multiselect__option--selected.multiselect__option--highlight {
+            background: $color-two;
+        }
+
+        .multiselect__content {
+            margin: 0;
+            list-style: none;
+            display: inline-block;
+            z-index: 100;
+        }
+
+        .multiselect__option--highlight {
+            background: $color-main;
+        }
+
+        .multiselect__select {
+            top: 2px;
+        }
+
+        .multiselect__single {
+            margin-bottom: 0;
+            padding-top: 3px;
+            color: $color-main;
+        }
+
+        .calc__head {
+            padding: 4px 4px 4px 0;
+            font-size: 14px;
+        }
+
+        .btn, button.btn {
+            display: inline-block;
+            text-align: center;
+            text-decoration: none;
+
+            margin: 2px 0;
+
+            color: #FFFFFF;
+            background-color: $color-main;
+
+            font-family: Arial, sans-serif;
+            font-size: 12px;
+            padding: 0 28px;
+
+            line-height: 40px;
+            height: 40px;
+
+            border-radius: 20px;
+            transition: all .2s ease;
+            border: 0;
+            outline: 0;
+        }
+
+        .btn:active {
+            transform: translateY(1px);
+            filter: saturate(150%);
+        }
+
+        .btn:hover {
+            /*color: #4C4C4C;*/
+            background-color: $color-two;
+            /*transform: translateY(1px);*/
+        }
+
+        .btn::-moz-focus-inner {
+            border: none;
+        }
+
+        .btn:focus {
+            outline: none;
+        }
+
+        button.btn--result {
+            font-family: Geometria, Arial, sans-serif;
+            font-size: 18px;
+            background-color: $color-two;
+            color: white;
+            height: 50px;
+            width: 100%;
+            margin-top: auto;
+            box-shadow: 10px 11px 50px -13px rgba(0, 0, 0, 0.75);
+        }
+        .calc__box {
+            display: flex;
+            align-items: left;
+            justify-content: flex-start;
+            flex-direction: column;
+            height: 100%;
+            font-family: Geometria, Arial, sans-serif;
+        }
+        @keyframes hvr-pop {
+            50% {
+                transform: scale(1.05);
+            }
+        }
+        .hvr-pop {
+            display: inline-block;
+            vertical-align: middle;
+            transform: perspective(1px) translateZ(0);
+            box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+        }
+        .hvr-pop:hover, .hvr-pop:focus, .hvr-pop:active {
+            animation-name: hvr-pop;
+            animation-duration: 0.4s;
+            animation-timing-function: linear;
+            animation-iteration-count: 1;
+        }
+        .calc__services {
+            font-size: 15px;
+            padding: 10px 10px;
+            border: #CDCDCD 1px solid;
+            border-radius: 10px;
+            margin-top: 7px;
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            justify-content: left;
+            transition: all .4s ease;
+            &:hover {
+                border: 1px solid #ee2f7b;
+                color: #ee2f7b;
+            }
+        }
+        .calc__plus {
+            font-size: 18px;
+            margin-left: auto;
+            margin-right: 10px;
+        }
+        .calc__price {
+            font-size: 35px;
+            padding-top: 7px;
+            text-align: right;
+            padding-right: 10px;
+        }
+        .calc__sum {
+            color: #ee2f7b;
         }
     }
-
-    .calc__preloader {
-        display: flex;
-        flex: 1;
-        align-items: center;
-        justify-content: center;
-        height: 100%;
-    }
-
-    #floor-calc .multiselect__content {
-        padding-left: 0;
-    }
-
-    .multiselect {
-        min-height: 33px;
-    }
-
-    .multiselect__tags {
-        min-height: 33px;
-        padding: 3px 33px 0 5px;
-        border: 1px solid rgba(60, 60, 60, .26);
-    }
-
-    .multiselect__select {
-        height: 33px;
-    }
-
-    .multiselect__content-wrapper {
-        z-index: 40;
-    }
-
-    .multiselect__element {
-        margin-bottom: 0;
-        z-index: 100;
-    }
-
-    .multiselect__option--selected.multiselect__option--highlight {
-        background: $color-two;
-    }
-
-    .multiselect__content {
-        margin: 0;
-        list-style: none;
-        display: inline-block;
-        z-index: 100;
-    }
-
-    .multiselect__option--highlight {
-        background: $color-main;
-    }
-
-    .multiselect__select {
-        top: 2px;
-    }
-
-    .multiselect__single {
-        margin-bottom: 0;
-        padding-top: 3px;
-    }
-
-    .calc__head {
-        padding: 5px 5px 5px 0;
-    }
-
 </style>
